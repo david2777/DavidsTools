a4dfd5d910ace56d7901a33377bb438077fe21c8
david2777 <daduvo11@gmail.com>
2016-02-15 20:50:57 -0500

    Initial Commit for Command Ports

---
+++ Maya/CommandPorts/SendCommand.py
@@ -0,0 +1,44 @@
+import socket
+
+def buildCommand(CommandList):
+    return "\n".join(CommandList)
+
+def SendCommand(Host, Port, Command):
+    """Takes a Command in the form of a list or string and sends it to the
+    specified Host and Port"""
+    #Verify Input
+    if type(Command) == str:
+        pass
+    elif type(Command) == list:
+        Command = buildCommand(Command)
+    else:
+        print "Error! Command must be string or list!"
+        return None
+    
+    #Establish connection    
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    conn = (Host, Port)
+    client.connect(conn)
+
+    #Send command and get response
+    client.send(Command)
+    response = client.recv(1024)
+    client.close()
+    
+    #Format, print and return response
+    response = str(response).replace("\n", "").replace("\x00", "").replace("\t", " ")
+    print '// Result: {0} //'.format(response)
+    return response
+
+if __name__=='__main__':
+    #You can send strings, notice the \\n to print a new line rather than
+    #   add a new line to the command.
+    MelCommand = """sphere;\nprint "Wow this works!\\n";\n"""
+    
+    #You can also send lists that will be turned into a command
+    PyCommand = ["import maya.cmds as cmds",
+                "cmds.sphere()",
+                "print 'Wow this works too!'"]
+
+    SendCommand("DavidPC", 5555, MelCommand)
+    SendCommand("DavidPC", 6666, PyCommand)
